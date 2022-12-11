f9371db95b79839477e747794862fc41b042c75e
박정아 <djdjejh@naver.com>
2022-12-11 21:01:03 +0900

    * build.gralde 수정(Spring Data JPA, h2 적용) * dto 테스트 등

--- src/main/java/com/jpaproject/springboot/web/HelloController.java
+++ src/main/java/com/jpaproject/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jpaproject.springboot.web;
 
+import com.jpaproject.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String Hello() {
         return "Hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jpaproject/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jpaproject/springboot/web/HelloControllerTest.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +28,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name ="hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name",name)
+                        .param("amount",String.valueOf(amount))) // param은 String만 가능해서 숫자/날짜등의 데이터도 등록할 때는 문자열만 가능
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
